c47bca5b2318a5eaae32f29402a673afa958dd72
Theimis <theimislimalopes422@gmail.com>
2026-01-07 15:17:28 -0300

    chore: atualizar site — ícones, rota base e README

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import Router from './Routes/routes.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/DevMovies">
       <Router />
     </BrowserRouter>
     <GlobalStyle />
